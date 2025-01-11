fb401e789ebc36cfbc6e35e00b7e36f5ea5c8c3d
Nicole Fischer <146454057+nicolefsc@users.noreply.github.com>
2025-01-11 14:36:40 +0100

    Tests eingebaut
    Test-driven programming

--- src/test/java/at/edu/c02/puzzleroom/CommandTest.java
+++ src/test/java/at/edu/c02/puzzleroom/CommandTest.java
@@ -1,12 +1,14 @@
 package at.edu.c02.puzzleroom;
 
+import at.edu.c02.puzzleroom.commands.CommandFastMove;
 import at.edu.c02.puzzleroom.commands.CommandLoad;
 import at.edu.c02.puzzleroom.commands.CommandMove;
+import at.edu.c02.puzzleroom.exceptions.PuzzleRoomException;
 import at.edu.c02.puzzleroom.exceptions.PuzzleRoomInvalidArgumentsException;
 import at.edu.c02.puzzleroom.exceptions.PuzzleRoomInvalidMoveException;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class CommandTest {
     @Test
@@ -39,5 +41,31 @@ public class CommandTest {
 
         // This should throw a PuzzleRoomInvalidArgumentsException
         new CommandMove(new String[]{"invalid"}).execute(gameBoard);
+    }
+    @Test
+    public void FastMovePosTest() throws PuzzleRoomException {
+        GameBoard gameBoard = new GameBoardImpl();
+        new CommandLoad(new String[]{"src/test/resources/simple.maze"}).execute(gameBoard);
+        Player player = gameBoard.getPlayer();
+
+        CommandFastMove cfm = new CommandFastMove(new String[]{"r", "r"});
+        cfm.execute(gameBoard);
+        assertTrue(gameBoard.isFinished());
+    }
+
+    @Test
+    public void FastMoveNegTest() throws PuzzleRoomException {
+        GameBoard gameBoard = new GameBoardImpl();
+        new CommandLoad(new String[]{"src/test/resources/simple.maze"}).execute(gameBoard);
+        Player player = gameBoard.getPlayer();
+
+        CommandFastMove cfm = new CommandFastMove(new String[]{"r", "u", "r"});
+        try {
+            cfm.execute(gameBoard);
+        } catch (Exception e) {
+            assertTrue(e instanceof PuzzleRoomInvalidMoveException);
+        }
+        assertFalse(gameBoard.isFinished());
+
     }
 }
